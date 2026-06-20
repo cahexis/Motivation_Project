afeb9a76f7ac52b0fec3026845c006c481db708e
Emi <183315760+cahexis@users.noreply.github.com>
2026-06-20 15:36:13 -0500

    tired emoji

--- MotivGui.py
+++ MotivGui.py
@@ -16,15 +16,11 @@ def updateval():
     print(current_val)
 
 
-
 #functions for adding/subtracting to the db value
 def addval():
     cursor.execute("UPDATE Applied_Jobs SET jobs = jobs + 1")
     connect.commit()
     updateval()
-
-    
-    
 
 def subval():
     cursor.execute("UPDATE Applied_Jobs SET jobs = jobs - 1")
@@ -38,13 +34,11 @@ root.title("Motivation :D")
 root.geometry('400x400')
 root.resizable(width=False, height=False)
 
-# Buttons
+# Buttons for adding/subtracting
 remove = tk.Button(root, text="-", font=('Verdana', 30),foreground='red', command=subval)
 remove.place(x=30,y=75)
 
 add = tk.Button(root, text="+", font=('Verdana', 30),foreground='green', command=addval)
 add.place(x=300,y=75)
-
-updateval()
 
 root.mainloop()
