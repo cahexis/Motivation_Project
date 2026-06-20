778f3a21b957194421e68ab27d06089c136c198e
Emi <183315760+cahexis@users.noreply.github.com>
2026-06-20 13:28:55 -0500

    Fixed up buttons, its ugly in my opinion but im no designer.

--- MotivGui.py
+++ MotivGui.py
@@ -7,13 +7,14 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Motivation :D")
 root.geometry('400x400')
+
 root.resizable(width=False, height=False)
 
 # Buttons
-remove = tk.Button(root, text="-")
-remove.place(x=100,y=100)
+remove = tk.Button(root, text="-", font=('Verdana', 30),foreground='red')
+remove.place(x=30,y=75)
 
-add = tk.Button(root, text="+")
-add.place(x=200,y=100)
+add = tk.Button(root, text="+", font=('Verdana', 30),foreground='green')
+add.place(x=300,y=75)
 
 root.mainloop()
