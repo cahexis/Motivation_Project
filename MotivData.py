33464513003202bec8b701da338af9f72aca4e6c
Emi <183315760+cahexis@users.noreply.github.com>
2026-06-20 14:48:11 -0500

    Finally setup starting data, data code and changing the value in the GUI py file

--- MotivData.py
+++ MotivData.py
@@ -2,4 +2,16 @@ import sqlite3
 
 con = sqlite3.connect('applied.db')
 curs = con.cursor()
-curs.execute('CREATE TABLE Applied_Jobs (jobs)')
+curs.execute('CREATE TABLE IF NOT EXISTS Applied_Jobs (jobs INTEGER)')
+
+curs.execute("SELECT jobs FROM Applied_Jobs")
+row = curs.fetchone()
+
+if row is None:
+    curs.execute("INSERT INTO Applied_Jobs (jobs) VALUES (0)")
+    con.commit()
+    print("Starting Value Set!")
+else:
+    print("No need to set starting!")
+con.commit()
+
--- MotivGui.py
+++ MotivGui.py
@@ -1,20 +1,50 @@
 import tkinter as tk
+import sqlite3
+
+#reference db from other file
+connect = sqlite3.connect("applied.db")
+cursor = connect.cursor()
+
+
+def getcurrentval():
+    cursor.execute("SELECT jobs FROM Applied_Jobs")
+    row = cursor.fetchone()
+    return row[0] if row else 0
+
+def updateval():
+    current_val = getcurrentval()
+    print(current_val)
 
 
 
+#functions for adding/subtracting to the db value
+def addval():
+    cursor.execute("UPDATE Applied_Jobs SET jobs = jobs + 1")
+    connect.commit()
+    updateval()
+
+    
+    
+
+def subval():
+    cursor.execute("UPDATE Applied_Jobs SET jobs = jobs - 1")
+    connect.commit()
+    updateval()
+      
 
 #Main window
 root = tk.Tk()
 root.title("Motivation :D")
 root.geometry('400x400')
-
 root.resizable(width=False, height=False)
 
 # Buttons
-remove = tk.Button(root, text="-", font=('Verdana', 30),foreground='red')
+remove = tk.Button(root, text="-", font=('Verdana', 30),foreground='red', command=subval)
 remove.place(x=30,y=75)
 
-add = tk.Button(root, text="+", font=('Verdana', 30),foreground='green')
+add = tk.Button(root, text="+", font=('Verdana', 30),foreground='green', command=addval)
 add.place(x=300,y=75)
+
+updateval()
 
 root.mainloop()
